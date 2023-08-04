80c80486c8227fa9a01a2096bf227fb0af20353a
Muhammad M. Mostey <79116164+MuhammadMostey@users.noreply.github.com>
2023-08-04 19:43:19 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,14 @@
 class PageHandler {
-    constructor(){
-        if(localStorage.getItem("theme") == "dark"){
-            document.documentElement.classList.add("dark");
-        }
+    constructor() {
+      if (localStorage.getItem("theme") == "dark") {
+        document.documentElement.classList.add("dark");
+      }
     }
 }
+
+const init = () => {
+    new PageHandler();
+};
+      
+
+document.addEventListener("DOMContentLoaded", () => init());
