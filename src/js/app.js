ec981476144f17876c06245c56fc4858ecb5bd51
Muhammad M. Mostey <79116164+MuhammadMostey@users.noreply.github.com>
2023-08-07 10:00:16 +0300

    updated the iconswtich for the dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,8 @@ class PageHandler {
         document.documentElement.classList.add("dark");
       }
 
+      this.setThemeIcon();
+      
       document.querySelector("#colorSwitch").addEventListener("click", () => {
         document.documentElement.classList.toggle("dark");
 
@@ -14,10 +16,36 @@ class PageHandler {
         else {
           localStorage.setItem("theme", "dark");
         }
+
+        this.setThemeIcon();
+
       });
 
     }
 
+    
+    setThemeIcon(){
+
+        let colorSwitch = document.querySelector("#colorSwitch");
+        if (colorSwitch === null) return;
+        
+        
+        if(localStorage.getItem("theme") == "dark"){
+
+            colorSwitch.innerHTML = `
+
+                <img src="/icons/sun.svg" alt="sun" class="w-5 h-5" />
+            
+            `
+
+        } else{
+            colorSwitch.innerHTML =`
+                <img src="/icons/moon.svg" alt="moon" class=" w-5 h-5" />
+            `
+
+        }
+
+    }
 
 }
 
