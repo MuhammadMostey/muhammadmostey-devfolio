225a67b74964c21a0c3bb6b4b571190f7371e919
Muhammad M. Mostey <79116164+MuhammadMostey@users.noreply.github.com>
2023-09-21 12:23:39 +0300

    updates the sortByDate funktion

--- src/utils/sortByDate.js
+++ src/utils/sortByDate.js
@@ -1,5 +1,4 @@
 // sortByDate function, sorts the blog article by the recent data for the blogs cards in the blog page
 export const sortByDate = (firstBlog, secondBlog) => {
-
     return Date(secondBlog.data.publishedDate) - Date(firstBlog.data.publishedDate);
 }
